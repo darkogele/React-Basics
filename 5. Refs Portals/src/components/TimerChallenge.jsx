b1d27a95518701a4edfadb044aa752fb610a7e2e
Darko Gelevski <darko.gelevski@deltatre.com>
2025-07-14 15:45:54 +0200

    feat: Implement TimerChallenge and ResultModal components with timer functionality

---
+++ 5. Refs Portals/src/components/TimerChallenge.jsx
@@ -0,0 +1,49 @@
+import { useRef, useState } from "react";
+import ResultModal from "./ResultModal";
+
+export default function TimerChallenge({ title, targetTime }) {
+  const timer = useRef();
+  const dialog = useRef();
+
+  const [timerExpired, setTimerExpired] = useState(false);
+  const [timerStarted, setTimerStarted] = useState(false);
+
+  function handleStart() {
+
+    timer.current = setTimeout(() => {
+      setTimerExpired(true);
+      dialog.current.showModal();
+      // alert(`Challenge "${title}" completed!`);
+    }, targetTime * 1000); // Simulate a timer
+
+    setTimerStarted(true);
+  }
+
+  function handleStop() {
+    // setTimerExpired(false);
+    // setTimerStarted(false);
+    clearTimeout(timer.current);
+  }
+
+  return (
+    <>
+      <ResultModal ref={dialog} targetTime={targetTime} result="lost" />
+
+      <section className="challenge">
+        <h2>{title}</h2>
+        <p className="challenge-time">
+          {targetTime} second{targetTime > 1 ? 's' : ''}
+        </p>
+        <p className="">
+          <button onClick={timerStarted ? handleStop : handleStart} disabled={timerExpired}>
+            {timerStarted ? ' Stop' : 'Start'} Challenge
+          </button>
+        </p>
+        <p className={timerStarted ? 'active' : undefined}>
+          {timerStarted ? 'Time is running...' : 'Timer inactive'}
+        </p>
+
+      </section>
+    </>
+  )
+}
