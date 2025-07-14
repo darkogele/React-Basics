af1761c24377008354e229da6d828c025dbccd88
Darko Gelevski <darko.gelevski@deltatre.com>
2025-07-14 16:32:29 +0200

    feat: Add onClose handler to ResultModal dialog for improved user experience

--- 5. Refs Portals/src/components/ResultModal.jsx
+++ 5. Refs Portals/src/components/ResultModal.jsx
@@ -16,7 +16,7 @@ export default function ResultModal({ ref, targetTime, remainingTime, onReset })
   });
 
   return (
-    <dialog ref={dialog} className="result-modal">
+    <dialog ref={dialog} className="result-modal" onClose={onReset}>
       {userLost && <h2>You Lost</h2>}
       {!userLost && <h2>Your Score: {score}</h2>}
 
