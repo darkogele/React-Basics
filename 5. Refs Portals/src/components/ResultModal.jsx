76f8b63a5782c145f5fa184e85ebaef3c38fbe78
Darko Gelevski <darko.gelevski@deltatre.com>
2025-07-14 16:38:29 +0200

    feat: Refactor ResultModal to use createPortal for rendering the dialog

--- 5. Refs Portals/src/components/ResultModal.jsx
+++ 5. Refs Portals/src/components/ResultModal.jsx
@@ -1,4 +1,5 @@
 import { useImperativeHandle, useRef } from "react";
+import { createPortal } from 'react-dom';
 
 export default function ResultModal({ ref, targetTime, remainingTime, onReset }) {
   const dialog = useRef(ref);
@@ -15,7 +16,7 @@ export default function ResultModal({ ref, targetTime, remainingTime, onReset })
     };
   });
 
-  return (
+  return createPortal(
     <dialog ref={dialog} className="result-modal" onClose={onReset}>
       {userLost && <h2>You Lost</h2>}
       {!userLost && <h2>Your Score: {score}</h2>}
@@ -26,6 +27,7 @@ export default function ResultModal({ ref, targetTime, remainingTime, onReset })
       <form method="dialog" onSubmit={onReset}>
         <button>Close</button>
       </form>
-    </dialog>
+    </dialog>,
+    document.getElementById('modal')
   )
 }
